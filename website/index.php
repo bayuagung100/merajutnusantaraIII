39ac4f06af78737fd83ede048ed8155f7f5c970b
bayuagung100 <bayuagung100@gmail.com>
2020-02-19 13:29:37 +0700

    revisi

--- website/index.php
+++ website/index.php
@@ -73,12 +73,16 @@ require 'vendor/autoload.php';
 	  </nav>
     <!-- END nav -->
     
-    <div class="hero-wrap js-fullheight" style="background-image: url('<?php echo base_url("images/icon/logo-merajut-nusantara4.png");?>');background-position: center center;">
+    <div class="hero-wrap js-fullheight" style="background-image: url('<?php echo base_url("images/icon/logo-revisi.png");?>');background-position: center center;">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
-          <div class="col-xl-10 ftco-animate" data-scrollax=" properties: { translateY: '70%' }"><b>
-            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"> <span>Merajut<br> Nusantara III 2020</span></h1></b>
+          <div class="col-xl-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+		  	<!-- <b style="text-align:right">
+            	<h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">
+				<span>Merajut<br>Nusantara<br>III 2020</span></h1>
+			</b> -->
+			<br><br><br><br><br><br><br><br><br>
             <div id="timer" class="d-flex mb-3">
 				<div class="time" id="days"></div>
 				<div class="time pl-4" id="hours"></div>
